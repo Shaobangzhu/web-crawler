755963d158cf911f2e5a08eea9ae9e2f11845f0d
Chaoran Lu <andyluloveusc@gmail.com>
2025-01-04 23:06:33 -0800

    set-up data storage and remove the test for now

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,16 +1,27 @@
 import superagent from "superagent";
 import * as cheerio from "cheerio";
+import fs from 'fs';
+import path from 'path';
 
 interface Course {
     title: string;
     count: number;
 }
 
+interface ResultObj {
+  time: number,
+  data: Course[]
+}
+
+interface Content {
+  [propName: number]: Course[];
+}
+
 export class Crawler {
   private _secret = "x3b174jsx";
   private _url = `http://www.dell-lee.com/typescript/demo.html?secret=${this._secret}`;
 
-  getJsonInfo(html: string | undefined) {
+  getJsonInfo(html: string) {
     try {
       if (!html) {
         throw new Error("HTML content is undefined or null.");
@@ -28,7 +39,7 @@ export class Crawler {
         time: new Date().getTime(),
         data: courseInfos
       };
-      console.log(result);
+      return result;
     } catch (error) {
         console.error("HTML content issue:", error);
     }
@@ -48,9 +59,20 @@ export class Crawler {
     }
   }
 
+  generateJsonContent(courseInfo: ResultObj) {
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+  }
+
   async initSpyderProcess() {
-    const html: string | undefined = await this.getRawHtml();
-    this.getJsonInfo(html);
+    const html = await this.getRawHtml() as string;
+    const courseInfo = this.getJsonInfo(html) as ResultObj;
+    this.generateJsonContent(courseInfo);
   }
 
   constructor() {
