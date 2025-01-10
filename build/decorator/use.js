684aa185a8b601f84b46626234fa1312bf241b38
Chaoran Lu <andyluloveusc@gmail.com>
2025-01-09 19:04:10 -0800

    remove content in the build folder

--- build/decorator/use.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.use = use;
-require("reflect-metadata");
-function use(middleware) {
-    return (target, key) => {
-        Reflect.defineMetadata("middleware", middleware, target, key);
-    };
-}
