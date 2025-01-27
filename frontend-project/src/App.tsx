f1d456cc27eacaf60d3c45d6693b49608d75eca8
Chaoran Lu <andyluloveusc@gmail.com>
2025-01-26 22:24:34 -0800

    import antd and implement form with it

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Form, Input, Button } from "antd";
+import { LockOutlined } from "@ant-design/icons";
+import "./login.css";
 
 function App() {
+  const [form] = Form.useForm();
+
+  const handleSubmit = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
-    <div className="App">
-      Hello World!
+    <div className="login-page">
+      <Form 
+        form={form} 
+        onFinish={handleSubmit} 
+        className="login-form"
+        initialValues={{ remember: true }} 
+      >
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please Enter LogIn Password" }]}
+        >
+          <Input.Password
+            prefix={<LockOutlined style={{ color: "rgba(0, 0, 0, .25)" }} />}
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            LogIn
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
